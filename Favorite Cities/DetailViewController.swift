1721e20236330ab145cbb40308897a8fe66ad73a
Patrick Dowell <patrickdowell@patrick-ds-macbook-air.local>
2019-02-04 11:24:36 -0600

    Added alert view

--- Favorite Cities/DetailViewController.swift
+++ Favorite Cities/DetailViewController.swift
@@ -21,8 +21,6 @@ class DetailViewController: UIViewController {
         }
     }
 
- 
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
