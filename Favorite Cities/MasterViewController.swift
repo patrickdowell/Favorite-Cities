e21dcd5fe4e47ebb36dbd2af6742bb367ce5b325
Patrick Dowell <patrickdowell@patrick-ds-macbook-air.local>
2019-02-05 11:30:03 -0600

    Enabled data persistence

--- Favorite Cities/MasterViewController.swift
+++ Favorite Cities/MasterViewController.swift
@@ -12,7 +12,7 @@ class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
     var cities = [City]()
-
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +25,18 @@ class MasterViewController: UITableViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        if let savedData = defaults.object(forKey: "Data") as? Data {
+            if let decoded = try? JSONDecoder().decode([City].self, from: savedData){
+                cities = decoded
+            }
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
         tableView.reloadData()
+        self.saveData()
     }
 
     @objc
@@ -63,6 +69,7 @@ class MasterViewController: UITableViewController {
                 self.cities.append(city)
                 self.tableView.reloadData()
             }
+            self.saveData()
         }
         alert.addAction(insertAction)
         present(alert, animated: true, completion: nil)
@@ -109,6 +116,7 @@ class MasterViewController: UITableViewController {
         if editingStyle == .delete {
             cities.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            self.saveData()
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
@@ -117,7 +125,13 @@ class MasterViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let objectToMove = cities.remove(at: sourceIndexPath.row)
         cities.insert(objectToMove, at: destinationIndexPath.row)
+        self.saveData()
+    }
+ 
+    func saveData() {
+        if let encoded = try? JSONEncoder().encode(cities) {
+            defaults.set(encoded, forKey: "data")
+        }
     }
     
 }
-
